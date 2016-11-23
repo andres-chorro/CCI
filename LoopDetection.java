1bd6130329bac15b1d6577d64ca333f4b7070240
chorro73 <chorro73@gmail.com>
2016-11-23 11:17:32 -0800

    Finish both implementations of loop detection

--- LoopDetection.java
+++ LoopDetection.java
@@ -39,16 +39,51 @@ class LoopDetection {
         Node end = new Node(8);
         end.next = answer;
         head.appendToTail(end);
-        
+
         Node curr = head;
         for (int i = 0; i < 17; i++) {
             System.out.println(curr.data);
             curr = curr.next;
         }
 
+        Node loopHead = loopHead(head);
+        System.out.println("loop head is: " + loopHead.data);
+        System.out.println("Loop head (no set) is: " + startNode(head).data);
+
     }
 
-    // public static Node loopHead(Node n) {
+    public static Node loopHead(Node n) {
+        HashSet<Node> set = new HashSet<>();
+        Node curr = n;
+        while (n.next != null && !set.contains(curr)) {
+            set.add(curr);
+            curr = curr.next;
+        }
+        return curr;
+    }
 
-    // }
+    public static Node startNode(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (fast == null || fast.next == null)
+            return null;
+
+        slow = head;
+
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
 }
