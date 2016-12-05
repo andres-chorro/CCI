8aca70ba7b632ac1602d66c0b1db64f02e68111e
chorro73 <chorro73@gmail.com>
2016-12-05 11:57:12 -0800

    Finish wrong (but working) multiplication function (to be replaced)

--- Operations.java
+++ Operations.java
@@ -1,13 +1,27 @@
 import java.util.*;
 
 public class Operations {
+
+    /**
+    * Multiplication using only addition.
+    * TODO: Fix this. It uses multiplication
+    */
     public static int multiply(int a, int b) {
-        // TODO: Finish
-        return 0;
+        boolean negative = ((a >>> 31) ^ (b >>> 31)) == 1;
+        a = Math.abs(a);
+        b = Math.abs(b);
+        int result = 0;
+        for (int i = 0; i < b; i++) {
+            result += a;
+        }
+        if (negative) {
+            result *= -1;
+        }
+        return result;
     }
 
     public static int subtract(int a, int b) {
-        // TODO: Finish
+
         return 0;
     }
 
@@ -18,6 +32,7 @@ public class Operations {
 
     public static void main(String[] args) {
         System.out.println(multiply(5, 6));
+        System.out.println(Integer.MAX_VALUE + 1);
         System.out.println(subtract(11, 23));
         System.out.println(divide(30, 15));
     }
