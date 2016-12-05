a299c62d4f533e82cf2efb310f04c3b2baec1bd1
chorro73 <chorro73@gmail.com>
2016-12-05 11:30:14 -0800

    Make skeleton for Operations.java

---
+++ Operations.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Operations {
+    public static int multiply(int a, int b) {
+        // TODO: Finish
+        return 0;
+    }
+
+    public static int subtract(int a, int b) {
+        // TODO: Finish
+        return 0;
+    }
+
+    public static int divide(int a, int b) {
+        // TODO: Finish
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiply(5, 6));
+        System.out.println(subtract(11, 23));
+        System.out.println(divide(30, 15));
+    }
+}
