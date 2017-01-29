65b313afa43dda183bb0443e6bf7aeecde2b9480
chorro73 <chorro73@gmail.com>
2017-01-29 12:15:40 -0800

    Start SortedMatrixSort

--- SortedMatrixSearch.java
+++ SortedMatrixSearch.java
@@ -1,6 +1,7 @@
 
 public class SortedMatrixSearch {
 	private int[][] matrix;
+	private static final Coord NOT_FOUND = new Coord(-1, -1);
 	
 	public SortedMatrixSearch(int[][] matrix) {
 		this.matrix = matrix;
@@ -8,19 +9,38 @@ public class SortedMatrixSearch {
 	
 	public Coord search(int target) {
 		if (matrix.length == 0 || matrix[0].length == 0) {
-			return new Coord(-1, -1);
+			return NOT_FOUND;
 		}
 		Coord topLeft = new Coord(0, 0); 
-		Coord bottomRight = new Coord(matrix.length, matrix[0].length);
+		Coord bottomRight = new Coord(matrix.length - 1, matrix[0].length - 1);
 		return searchHelper(target, topLeft, bottomRight);
 	}
 	
 	private Coord searchHelper(int target, Coord topLeft, Coord bottomRight) {
+		if (topLeft.equals(bottomRight)) {
+			return get(topLeft) == target ? topLeft : NOT_FOUND;
+		}
+		
 		Coord mid = new Coord((topLeft.row+bottomRight.row) / 2, (topLeft.col+bottomRight.col) / 2);
 		if (target == get(mid)) {
 			return mid;
 		}
-		return new Coord(-1, -1);
+		if (target < get(mid)) {
+			Coord topBlockResult = searchHelper(target, topLeft, new Coord(mid.row-1, bottomRight.col));
+			if (topBlockResult != NOT_FOUND)
+				return topBlockResult;
+			Coord leftBlockResult = searchHelper(target, new Coord(mid.row, topLeft.col), new Coord(bottomRight.row, mid.col-1));
+			if (leftBlockResult != NOT_FOUND)
+				return leftBlockResult;
+		} else {
+			Coord bottomBlockResult = searchHelper(target, new Coord(mid.row+1, topLeft.col), bottomRight);
+			if (bottomBlockResult != NOT_FOUND)
+				return bottomBlockResult;
+			Coord rightBlockResult = searchHelper(target, new Coord(topLeft.row, mid.col+1), new Coord(mid.row, bottomRight.col));
+			if (rightBlockResult != NOT_FOUND)
+				return rightBlockResult;
+		}
+		return NOT_FOUND;
 	}
 	
 	private int get(Coord c) {
@@ -35,5 +55,16 @@ class Coord {
 	Coord(int row, int col) {
 		this.row = row;
 		this.col = col;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Coord other = (Coord) obj;
+		return row == other.row && col == other.col;
+	};
+	
+	@Override
+	public String toString() {
+		return "(" + row + ", " + col + ")";
 	}
 }
--- SortedMatrixSearchTest.java
+++ SortedMatrixSearchTest.java
@@ -39,5 +39,30 @@ public class SortedMatrixSearchTest {
 		assertEquals(-1, found.row);
 		assertEquals(-1, found.col);
 	}
+	
+	@Test
+	public void simpleNotInDiagonal() {
+		int[][] matrix = {{1, 5, 7}, {3, 12, 20}, {5, 13, 25}, {12, 14, 30}};
+		SortedMatrixSearch m = new SortedMatrixSearch(matrix);
+		Coord found = m.search(13);
+		assertEquals(2, found.row);
+		assertEquals(1, found.col);
+	}
+	@Test
+	public void simpleNotInDiagonal2() {
+		int[][] matrix = {{1, 5, 7}, {3, 12, 20}, {5, 13, 25}, {12, 14, 30}};
+		SortedMatrixSearch m = new SortedMatrixSearch(matrix);
+		Coord found = m.search(3);
+		assertEquals(1, found.row);
+		assertEquals(0, found.col);
+	}
+	@Test
+	public void simpleNotInDiagonal3() {
+		int[][] matrix = {{1, 5, 7}, {3, 12, 20}, {5, 13, 25}, {12, 14, 30}};
+		SortedMatrixSearch m = new SortedMatrixSearch(matrix);
+		Coord found = m.search(1);
+		assertEquals(0, found.row);
+		assertEquals(0, found.col);
+	}
 
 }
