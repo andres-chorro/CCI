bcaf06075238ddf77e9ec6d1ba3754f89fcc03f0
chorro73 <chorro73@gmail.com>
2017-02-14 13:04:20 -0800

    Finish 4.1 Route between nodes and tests.

--- mygraph/MyGraph.java
+++ mygraph/MyGraph.java
@@ -1,72 +1,95 @@
 package mygraph;
+
 import java.util.*;
 
 public class MyGraph {
 
-    public List<Node> nodes;
-    public HashMap<Node, Node> parents;
+	public List<Node> nodes;
+	public HashMap<Node, Node> parents;
 
-    public MyGraph(LinkedList<Node> nodes) {
-        this.nodes = nodes;
-    }
-    
-    public void dfs() {
-    	parents = new HashMap<>();
-    	for (Node n : nodes) {
-    		if (!parents.containsKey(n)) {
-    			parents.put(n, null);
-    			dfsVisit(n);
-    		}
-    	}
-    }
-    
-    public void dfsVisit(Node n) {
-    	for (Node child : n.children) {
-    		if (!parents.containsKey(child)) {
-    			parents.put(child, n);
-    			dfsVisit(child);
-    		}
-    	}
-    }
-    
-    
-    public void bfs() {
-    	parents = new HashMap<>();
-    	for (Node n : nodes) {
-    		if (!parents.containsKey(n)) {
-    			parents.put(n, null);
-    			bfsVisit(n);
-    		}
-    	}
-    }
-    
-    public void bfsVisit(Node root) {
-    	LinkedList<Node> queue = new LinkedList<>();
-    	queue.add(root);
-    	
-    	while (!queue.isEmpty()) {
-    		Node p = queue.pop();
-    		for (Node c : p.children) {
-    			if (!parents.containsKey(c)) {
-    				parents.put(c, p);
-    				queue.add(c);
-    			}
-    		}
-    	}
-    }
+	public MyGraph(LinkedList<Node> nodes) {
+		this.nodes = nodes;
+	}
+
+	public void dfs() {
+		parents = new HashMap<>();
+		for (Node n : nodes) {
+			if (!parents.containsKey(n)) {
+				parents.put(n, null);
+				dfsVisit(n);
+			}
+		}
+	}
+
+	public void dfsVisit(Node n) {
+		for (Node child : n.children) {
+			if (!parents.containsKey(child)) {
+				parents.put(child, n);
+				dfsVisit(child);
+			}
+		}
+	}
+
+	public void bfs() {
+		parents = new HashMap<>();
+		for (Node n : nodes) {
+			if (!parents.containsKey(n)) {
+				parents.put(n, null);
+				bfsVisit(n);
+			}
+		}
+	}
+
+	public void bfsVisit(Node root) {
+		LinkedList<Node> queue = new LinkedList<>();
+		queue.add(root);
+
+		while (!queue.isEmpty()) {
+			Node p = queue.pop();
+			for (Node c : p.children) {
+				if (!parents.containsKey(c)) {
+					parents.put(c, p);
+					queue.add(c);
+				}
+			}
+		}
+	}
+
+	public boolean pathBetweenExists(Node a, Node b) {
+		if (a == b) {
+			return true;
+		}
+		LinkedList<Node> queue = new LinkedList<>();
+		HashSet<Node> seen = new HashSet<>();
+		queue.add(a);
+		seen.add(a);
+		while (queue.isEmpty() == false) {
+			Node p = queue.pop();
+			for (Node c : p.children) {
+				if (c == b) {
+					return true;
+				}
+				if (seen.contains(c) == false) {
+					queue.add(c);
+					seen.add(c);
+				}
+			}
+		}
+		return false;
+	}
 }
 
 class Node {
-    public String name;
-    public LinkedList<Node> children;
+	public String name;
+	public LinkedList<Node> children;
 
-    public Node(String name, LinkedList<Node> children) {
-        this.name = name;
-        this.children = children;
-    }
-    
-    @Override
-    public String toString() {
-    	return name;
-    }
+	public Node(String name, LinkedList<Node> children) {
+		this.name = name;
+		this.children = children;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
 }
--- mygraph/MyGraphTest.java
+++ mygraph/MyGraphTest.java
@@ -8,14 +8,15 @@ import org.junit.Test;
 public class MyGraphTest {
 	
 	MyGraph graph;
+	Node a, b, c, d, e, f;
 	@Before
 	public void setup() {
-		Node a = new Node("a", new LinkedList<Node>());
-		Node b = new Node("b", new LinkedList<Node>());
-		Node c = new Node("c", new LinkedList<Node>());
-		Node d = new Node("d", new LinkedList<Node>());
-		Node e = new Node("e", new LinkedList<Node>());
-		Node f = new Node("f", new LinkedList<Node>());
+		a = new Node("a", new LinkedList<Node>());
+		b = new Node("b", new LinkedList<Node>());
+		c = new Node("c", new LinkedList<Node>());
+		d = new Node("d", new LinkedList<Node>());
+		e = new Node("e", new LinkedList<Node>());
+		f = new Node("f", new LinkedList<Node>());
 		a.children.add(b);
 		a.children.add(d);
 		b.children.add(e);
@@ -44,6 +45,31 @@ public class MyGraphTest {
 	public void testBFS() {
 		graph.bfs();
 		System.out.println("BFS: " + graph.parents);
+	}
+	
+	@Test
+	public void testGoodPaths() {
+		assertTrue(graph.pathBetweenExists(a, b));
+		assertTrue(graph.pathBetweenExists(a, e));
+		assertTrue(graph.pathBetweenExists(a, d));
+		assertTrue(graph.pathBetweenExists(b, d));
+		assertTrue(graph.pathBetweenExists(b, e));
+		assertTrue(graph.pathBetweenExists(c, e));
+		assertTrue(graph.pathBetweenExists(c, d));
+		assertTrue(graph.pathBetweenExists(c, f));
+	}
+	
+	@Test
+	public void testSameNodeGoodPath() {
+		assertTrue(graph.pathBetweenExists(a, a));
+		assertTrue(graph.pathBetweenExists(f, f));
+	}
+	
+	@Test
+	public void testBadPaths() {
+		assertFalse(graph.pathBetweenExists(a, f));
+		assertFalse(graph.pathBetweenExists(b, a));
+		assertFalse(graph.pathBetweenExists(f, b));
 	}
 
 }
